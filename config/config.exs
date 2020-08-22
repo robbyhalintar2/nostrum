6dad333391627a65b5ddc15a6bd6eab5ccb19069
robbyhalintar2 <67970287+robbyhalintar2@users.noreply.github.com>
2020-08-22 14:54:48 +0700

    Update config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :nostrum,
-  token: "",
+  token: "NzQ2NjI5NDE3MzQ1NzQ0OTE2.X0DG1A.OMLE7jwiYWO0SIZeKrFDKUkCtpE",
   num_shards: :auto
 
 config :logger, :console, metadata: [:shard]
